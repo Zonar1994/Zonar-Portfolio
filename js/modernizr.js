7f857b1cd0b8f43ea45490fb70eb77f8350529a7
Zonar1994 <zonar199421@gmail.com>
2023-12-06 22:57:47 +0100

    updated xtot and learning

--- js/modernizr.js
+++ js/modernizr.js
@@ -292,6 +292,33 @@ window.Modernizr = (function( window, document, undefined ) {
         return bound;
       };
     }
+    document.getElementById("image.zoom").onclick = function() {
+      if (this.style.width === "100px") {
+          this.style.width = "500px";  // Change to the desired enlarged size
+      } else {
+          this.style.width = "100px";  // Change back to the smaller size
+      }
+  };
+  // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal
+var img = document.getElementById("image.zoom");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
 
     document.addEventListener("DOMContentLoaded", function() {
       setTimeout(function() {
